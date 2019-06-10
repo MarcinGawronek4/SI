bf41c8d9a0a3133f66b2f24746f9bedfec6c03eb
DamianKuich <43881268+DamianKuich@users.noreply.github.com>
2019-06-10 13:35:17 +0200

    Update D_tree_bill.py

--- D_tree_bill.py
+++ D_tree_bill.py
@@ -5,28 +5,45 @@ import pandas as ps
 import numpy as np
 
 
+def BuildTree():
+    # nazwy cech
+    feature_names = ["pair", "empty_plate", "talking", "mood", "asked", "hurry", "bill"]
 
-# nazwy cech
-feature_names = ["pair", "empty_plate", "talking", "mood", "asked", "hurry", "bill"]
+    Yfeature_names = ["pair", "empty_plate", "talking", "mood", "asked", "hurry"]
 
-Yfeature_names = ["pair", "empty_plate", "talking", "mood", "asked", "hurry"]
+    # wczytaj dataset z pliku dane.csv
+    dataset = ps.read_csv("bill.csv", header=None, names=feature_names, sep=";")
 
-# wczytaj dataset z pliku dane.csv
-dataset = ps.read_csv("bill.csv", header=None, names=feature_names, sep=";")
+    X = dataset.drop('bill', axis=1)
+    Y = dataset['bill']
 
-X = dataset.drop('bill', axis=1)
-Y = dataset['bill']
+    # tworzenie drzewa decyzyjnego
+    clf = Id3Estimator()
 
-# tworzenie drzewa decyzyjnego
-clf = Id3Estimator()
+    # Podział na dane treningowe i dane testowe
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+    # fit - synonim do "find patterns in data"
+    clf.fit(X_train, Y_train)
+    export_graphviz(clf.tree_, "test.dot", feature_names)
 
-# Podział na dane treningowe i dane testowe
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
-# fit - synonim do "find patterns in data"
-clf.fit(X_train, Y_train)
-export_graphviz(clf.tree_, "test.dot", feature_names)
+    return clf
 
-print(clf.predict(X_test))
+
+def PredictBill(clf,prediction):
+    clientInfo = [
+        [np.random.randint(2), prediction, np.random.randint(2), np.random.randint(99), np.random.randint(2),
+         np.random.randint(2)]]
+    print(clientInfo)
+    result = clf.predict(clientInfo)
+    print("Czy wydac rachunek: ", result)
+    return result
+
+
+# bill = BuildTree()
+# if PredictBill(bill) == 1:
+#     print("udalo sie")
+# else:
+#     print("no przyps")
 
 
 
